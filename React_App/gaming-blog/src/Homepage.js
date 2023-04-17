556fb56d9c888a30871bc45c1b94eac9d48a5795
Onkar Karale <onkarkarale4@gmail.com>
2023-04-17 22:40:32 +0530

    Intro to React

--- React_App/gaming-blog/src/Homepage.js
+++ React_App/gaming-blog/src/Homepage.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 const Homepage = () => {
-    return (
-        <h2>This is the Homepage</h2>
-    )
+
+  const clickHandler = () => {
+    console.log('Hello')
+  }
+
+  // const clickHandler = (e) => {
+  //   console.log(e);
+  //   console.log(e.target);
+  // }
+
+  const clickHandlerCustomized = (name) => {
+    console.log('Hello: ', name)
+  }
+
+  // let name = "Onkar";
+
+  // so, if u want to change the name to another name in the same place where first name was then, we have to import 'usestate'  from react
+  const [name, setName] = useState("Onkar")
+  const [Subscribers, setSubs] = useState("550000")
+
+  const clickToChangeName = () => {
+    setName("Rishi");
+  }
+
+  const clickToChangeSubs = () => {
+    setSubs("650000");
+  }
+
+  return (
+    <div className='Home'>
+      <h2>HOME PAGE</h2>
+      <p>{name}</p>
+      <p>{name} has {Subscribers} subscribers </p>
+      {/* so, if u want to change the first name to last name in the same place where first name was then we have to import 'usestate' */}
+      <button onClick={clickHandler}>
+        Click Me
+      </button>
+      <button onClick={() => { clickHandlerCustomized("onkar") }}>
+        Click Me Too
+      </button>
+      <button onClick={clickToChangeName}>
+        Update Name
+      </button>
+      <button onClick={clickToChangeSubs}>
+        Update Subscribers
+      </button>
+    </div >
+  )
 }
 
-export default Homepage;
+export default Homepage
--- React_App/gaming-blog/src/Navbar.js
+++ React_App/gaming-blog/src/Navbar.js
@@ -1,14 +1,20 @@
 const navbar = () => {
-    return ( <
-        nav className = "Navbar" >
-        <
-        h1 > Gaming Blog < /h1> <
-        div className = "links" >
-        <
-        a href = "./" > Home < /a>  &nbsp; &nbsp; <
-        a href = "./" > Blog < /a> <
-        /div> <
-        /nav>
+    return (
+        <nav className="Navbar">
+            <h1>Gaming Blog</h1>
+
+            <div className="links">
+                <a href="./" style={{            // Inline styling for blog,  blue curly-bracket is for the dynamic values and yellow is for the objects means key value pairs.
+                    color: "rgb(64, 191, 245)",
+                    borderBottom: "1px solid rgb(64, 191, 245)",
+                    borderRadius: "10px"
+                }}>Home</a>
+
+                < a href="./">Blog</a>
+                <a href="./">About Us</a>
+                <a href="./">Contact Us</a>
+            </div>
+        </nav>
     )
 }
 
